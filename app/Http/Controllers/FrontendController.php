fbab2d0b1832a95bf1b3ad299a491ad959c006a4
SimonSaputra29 <simonsaputra2908@gmail.com>
2025-02-06 14:38:13 +0700

    nambah kontak

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -8,35 +8,75 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    public function home(){
+    public function home()
+    {
         $configuration = Configuration::first();
-        $hero = Hero::first();
+        $hero = Hero::first(); 
+
         return view('frontend.pages.home', [
             'title' => 'Kursus Yasmin',
-        ], compact('configuration', 'hero'));
+            'configuration' => $configuration,
+            'hero' => $hero,
+        ]);
     }
 
-    public function langganan(){
+    public function langganan()
+    {
+        $configuration = Configuration::first();
+        $hero = Hero::first();
+
         return view('frontend.pages.langganan', [
-            'title'=> 'Langganan',
+            'title' => 'Langganan',
+            'configuration' => $configuration,
+            'hero' => $hero,
         ]);
     }
 
-    public function program(){
+    public function program()
+    {
+        $configuration = Configuration::first();
+        $hero = Hero::first();
+
         return view('frontend.pages.program', [
-            'title'=> 'Program',
+            'title' => 'Program',
+            'configuration' => $configuration,
+            'hero' => $hero,
         ]);
     }
 
-    public function learning(){
+    public function learning()
+    {
+        $configuration = Configuration::first();
+        $hero = Hero::first();
+
         return view('frontend.pages.learning-path', [
-            'title'=> 'Learning Path',
+            'title' => 'Learning Path',
+            'configuration' => $configuration,
+            'hero' => $hero,
         ]);
     }
 
-    public function tentang(){
+    public function tentang()
+    {
+        $configuration = Configuration::first();
+        $hero = Hero::first();
+
         return view('frontend.pages.tentang', [
-            'title'=> 'Tentang',
+            'title' => 'Tentang',
+            'configuration' => $configuration,
+            'hero' => $hero,
+        ]);
+    }
+
+    public function kontak()
+    {
+        $configuration = Configuration::first();
+        $hero = Hero::first();
+
+        return view('frontend.pages.kontak', [
+            'title' => 'Kontak',
+            'configuration' => $configuration,
+            'hero' => $hero,
         ]);
     }
 }
