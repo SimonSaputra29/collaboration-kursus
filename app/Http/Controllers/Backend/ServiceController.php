324648bd0307a995e83047b6495c212825352a46
akmalrn <legendsmystic60@gmail.com>
2025-02-12 14:48:37 +0700

    update

--- app/Http/Controllers/Backend/ServiceController.php
+++ app/Http/Controllers/Backend/ServiceController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Configuration;
+use App\Models\Backend\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $configuration = Configuration::first();
+        $services = Service::all();
+        return view('backend.database.service.index', compact('configuration', 'services'));
+    }
 }
--- resources/views/backend/database/service/index.blade.php
+++ resources/views/backend/database/service/index.blade.php
@@ -5,65 +5,32 @@
         <div class="card">
             <div class="card-header">
                 <div class="d-flex align-items-center">
-                    <h4 class="card-title">Gambar Keunggulan</h4>
+                    <h4 class="card-title">Services</h4>
                     <button class="btn btn-primary btn-round ms-auto" data-bs-toggle="modal" data-bs-target="#addRowModal">
                         <i class="fa fa-plus"></i>
-                        Tambah Gambar
+                        Add 
                     </button>
                 </div>
             </div>
             <div class="card-body">
-                <!-- Modal Tambah Gambar -->
-                <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header border-0">
-                                <h5 class="modal-title">
-                                    <span class="fw-mediumbold">New</span>
-                                    <span class="fw-light"> Gambar Keunggulan</span>
-                                </h5>
-                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <p class="small">Silakan unggah gambar keunggulan baru di sini.</p>
-                                <form method="POST" action="{{ route('superiorityImage.store') }}"
-                                    enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="form-group">
-                                        <label for="image">Pilih Gambar</label>
-                                        <input type="file" name="image" id="image"
-                                            class="form-control @error('image') is-invalid @enderror">
-                                        @error('image')
-                                            <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="modal-footer border-0">
-                                        <button type="submit" class="btn btn-primary">Tambah</button>
-                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-
                 <!-- Tabel Daftar Gambar -->
                 <div class="table-responsive">
                     <table id="add-row" class="display table table-striped table-hover">
                         <thead>
                             <tr>
-                                <th>Gambar</th>
-                                <th style="width: 10%">Aksi</th>
+                                <th>Category</th>
+                                <th>Title</th>
+                                <th>Overview</th>
+                                <th>Description</th>
+                                <th>link</th>
+                                <th style="width: 10%">Action</th>
                             </tr>
                         </thead>
                         <tfoot>
                             <tr>
-                                <th>Gambar</th>
-                                <th>Aksi</th>
+                                <th>Category</th>
+                                <th>Title</th>
+                                <th>Action</th>
                             </tr>
                         </tfoot>
                         <tbody>
